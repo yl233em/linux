07dcaecf30693cc78e8567eaf53fb1b06d2e2c61
yl233em <3156586673@qq.com>
2024-03-31 14:42:34 +0800

    update

--- 3.31/project2/myproc.c
+++ 3.31/project2/myproc.c
@@ -7,8 +7,10 @@ int main()
 	printf("I`m parent, pid:%d\n", getpid());
 	
 	pid_t ret = fork();
+	
+	printf("%d\n", ret);
 
-	printf("my pid:%d\n",getpid());
+	//printf("my pid:%d\n",getpid());
 	
 	sleep(1);
 	return 0;
