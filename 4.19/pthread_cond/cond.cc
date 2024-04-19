0877d4e5c996187b1b3b055f2186700c1387c851
yl233em <3156586673@qq.com>
2024-04-19 12:01:34 +0800

    同步

---
+++ 4.19/pthread_cond/cond.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<pthread>
+#include<unistd.h>
+
+#define TNUM 4
+typedef void (*func_t)(const std::string &name, pthread_mutex_t *pmutx, pthread_cond_t *pcond);
+
+
+
+
+
+int main()
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    pthread_cond_init(&cond, nullptr);
+    pthread_mutex_init(&mutex, nullptr);
+
+    pthread_t tids[TNUM];
+    tids[];
+
+
+    return 0;
+}
